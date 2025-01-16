5f037fa36af99d3acea84d2dd5b069afa9adcd06
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2025-01-16 12:34:47 +0700

    add toast notification

--- resources/views/layouts/excel.blade.php
+++ resources/views/layouts/excel.blade.php
@@ -12,5 +12,18 @@
   <body class="bg-white">
     @yield('content')
     @include('partials.admin-script')
-  </body>\
+    <script>
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+    </script>
+  </body>
 </html>
